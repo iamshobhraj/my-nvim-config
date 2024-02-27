6e107ec2f0c787afd35622fad8fa6f47ec7f8d2c
iamshobhraj <iamshobhraj@gmail.com>
2024-02-27 08:15:31 +0530

    my nvim config

---
+++ init.lua
@@ -0,0 +1,48 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.tabstop= 4
+vim.opt.shiftwidth= 4
+vim.opt.smarttab = true
+vim.opt.softtabstop= 4
+vim.opt.mouse= 'a'
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
